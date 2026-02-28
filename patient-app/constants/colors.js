f3ac11cff15dd3fb9a91bba17b27b11030b94965
Nhanvas <nqt.nhan2606@gmail.com>
2026-02-28 13:26:34 +0700

    Initial project setup

---
+++ patient-app/constants/colors.js
@@ -0,0 +1,11 @@
+export const Colors = {
+    bluePrimary: '#1892BE',
+    redPrimary: '#D4494F',
+    yellowPrimary: '#FFE262',
+    green: '#46C96B',
+    white: '#FFFFFF',
+    black900: '#212121',
+    black700: '#424242',
+    black500: '#757575',
+    gray100: '#F5F5F5',
+  };
---
+++ volunteer-app/constants/colors.js
@@ -0,0 +1,18 @@
+export const Colors = {
+    // Patient App
+    bluePrimary: '#1892BE',
+  
+    // Volunteer App  
+    redPrimary: '#D4494F',
+  
+    // Bystander Web
+    yellowPrimary: '#FFE262',
+  
+    // Shared
+    green: '#46C96B',
+    white: '#FFFFFF',
+    black900: '#212121',
+    black700: '#424242',
+    black500: '#757575',
+    gray100: '#F5F5F5',
+  };
